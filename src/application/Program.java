4b382ca2c5bd0c3a9cd7e96f9047084e256b328b
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-31 10:45:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Teste");
-        System.out.println(department);
+        Seller seller = new Seller(1, "João", "joao@gmail.com", new Date(), 2000.0, department);
+        System.out.println(seller);
     }
 }
